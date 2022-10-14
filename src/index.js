b1072c30a493437301d324af2f7c2a6125ab5ca0
Andrew Jennison <andrewjohnjennison@gmail.com>
2022-10-14 13:45:48 -0700

    create render.js module and add renderPage()

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import "./style.css";
+import { renderPage } from "./render.js";
+import { getWeatherData } from "./storage.js";
+
+getWeatherData();
---
+++ src/render.js
@@ -0,0 +1,100 @@
+import cloudyIcon from "./images/cloudyIcon-24w.png";
+import partlyCloudyIcon from "./images/partlyCloudyIcon-24w.png";
+import rainyIcon from "./images/rainyIcon-24w.png";
+import sunnyIcon from "./images/sunnyIcon-24w.png";
+import ghLogo from "./images/gitHubLogo-24w.png";
+import copyright from "./images/copyright-24w.png";
+
+// BEGIN RENDERING OF STATIC ELEMENTS
+
+export const renderPage = (() => {
+  const body = document.querySelector("body");
+  body.insertAdjacentHTML(
+    "afterbegin",
+    `<header>
+    <div class="title">Damp Detector</div>
+  </header>
+
+  <div class="mainContent">
+    <div class="location">Seattle</div>
+    <div class="temp">70° F</div>
+    <div class="buttonContainer">
+      <button class="changeLocation">Change Location</button>
+      <button class="toggleTempScale">Display Celsius</button>
+    </div>
+    <div class="conditions">Cloudy</div>
+    <div class="tempRangeContainer">
+      <div class="highTemp">H: 80°</div>
+      <div class="lowTemp">L: 60°</div>
+    </div>
+    <ul class="fiveDayForecast">
+      <li class="forecastContainer">
+        <div>Day 1</div>
+        <img class="weatherIcon" alt="sunny" />
+        <div>H: X° L: Y°</div>
+      </li>
+      <li class="forecastContainer">
+        <div>Day 2</div>
+        <img class="weatherIcon" alt="cloudy" />
+        <div>H: X° L: Y°</div>
+      </li>
+      <li class="forecastContainer">
+        <div>Day 3</div>
+        <img class="weatherIcon" alt="partly cloudy" />
+        <div>H: X° L: Y°</div>
+      </li>
+      <li class="forecastContainer">
+        <div>Day 4</div>
+        <img class="weatherIcon" alt="rainy" />
+        <div>H: X° L: Y°</div>
+      </li>
+      <li class="forecastContainer">
+        <div>Day 5</div>
+        <img class="weatherIcon" alt="sunny" />
+        <div>H: X° L: Y°</div>
+      </li>
+    </ul>
+
+    <div class="otherWeatherInfo">
+      <div class="other aqi">AQI PH</div>
+      <div class="other wind">Wind PH</div>
+      <div class="other humidity">Humidity PH</div>
+      <div class="other feelsLike">Feels Like PH</div>
+    </div>
+  </div>
+  <footer>
+    <a
+      href="https://github.com/thigmotaxis/damp-detector"
+      class="sourceContainer"
+    >
+      <img class="repoLogo" alt="the github logo" />
+      <div>View Source</div>
+    </a>
+    <div class="copyrightContainer">
+      <img class="copyrightSymbol" alt="the copyright symbol" />
+      <div>2022 Abe Industries</div>
+    </div>
+  </footer>`
+  );
+
+  const weatherIconFiles = [
+    sunnyIcon,
+    cloudyIcon,
+    partlyCloudyIcon,
+    rainyIcon,
+    sunnyIcon,
+  ];
+
+  const weatherIcons = document.querySelectorAll(".weatherIcon");
+  weatherIcons.forEach((element, index) => {
+    element.setAttribute("src", weatherIconFiles[index]);
+  });
+
+  const repoLogo = document.querySelector(".repoLogo");
+  repoLogo.setAttribute("src", ghLogo);
+
+  const copyrightSymbol = document.querySelector(".copyrightSymbol");
+  copyrightSymbol.setAttribute("src", copyright);
+})();
+
+// END RENDERING OF STATIC ELEMENTS
