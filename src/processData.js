24228767cb2b92a9677fd5ed0cddc4f4cf20273f
Andrew Jennison <andrewjohnjennison@gmail.com>
2022-10-19 14:24:55 -0700

    add getLocalDays()

--- src/processData.js
+++ src/processData.js
@@ -1,3 +1,5 @@
+import { add } from "date-fns";
+
 export const getCoordinates = (data) => {
   const coordinates = {
     location: data[0].name,
@@ -10,6 +12,7 @@ export const getCoordinates = (data) => {
 
 export const processWeatherData = (data) => {
   const processedWeatherData = {
+    localTimezoneOffset: data.timezone_offset / 3600,
     current: {
       tempF: `${Math.round(((data.current.temp - 273.15) * 9) / 5 + 32)}° F`,
       feelsLikeF: `Feels Like: ${Math.round(
@@ -117,7 +120,42 @@ export const processWeatherData = (data) => {
       )}° C  L: ${Math.round(data.daily[6].temp.min - 273.15)}° C`,
     },
   };
+
   console.log(data);
   console.log(processedWeatherData);
   return processedWeatherData;
+};
+
+export const getLocalDays = () => {
+  const localTimezoneOffset = JSON.parse(
+    window.localStorage.getItem("currentWeatherData")
+  ).localTimezoneOffset;
+  const myTimezoneOffset = new Date().getTimezoneOffset() / 60;
+  const differential = localTimezoneOffset + myTimezoneOffset;
+  const localDate = add(new Date(), { hours: differential });
+  const localDay = localDate.getDay();
+
+  const days = ["Today"];
+
+  for (let i = 1; i < 7; i++) {
+    // i is initialized at 1 because we do not want to retrieve today's name and this seems cleaner than (let day = (localDay + i + 1))
+    let day = (localDay + i) % 7; // % operand prevents error when day + i > 6
+    const dayName =
+      day === 0
+        ? "Sunday"
+        : day === 1
+        ? "Monday"
+        : day === 2
+        ? "Tuesday"
+        : day === 3
+        ? "Wednesday"
+        : day === 4
+        ? "Thursday"
+        : day === 5
+        ? "Friday"
+        : "Saturday";
+    days.push(dayName);
+  }
+
+  return days;
 };
